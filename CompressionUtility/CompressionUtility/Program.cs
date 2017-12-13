86584a4baf18b76f6f4539feb3425e0e713591a1
M Davies <matdavies@outlook.com>
2017-12-13 22:45:15 +0000

    Empty Shell

---
+++ CompressionUtility/CompressionUtility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompressionUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
